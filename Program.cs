82c20fc2cbe8c0aa9dbe4b62ed38407f4fb50e24
Samuel <samuelimagg@gmail.com>
2025-03-19 20:19:30 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Channels;
+using Discord;
+using Discord.WebSocket;
+
+DotNetEnv.Env.Load();
+var discordToken = Environment.GetEnvironmentVariable("DiscordToken");
+
+async Task RunBotAsync()
+{
+    var client = new DiscordSocketClient();
+    await client.LoginAsync(Discord.TokenType.Bot, discordToken);
+    Console.WriteLine(client.LoginState);
+
+    await client.StartAsync();
+
+    client.Ready += async () => 
+    {
+        var guild = client.GetGuild(1352036217780306083);
+        var channel = guild.GetTextChannel(1352036218828751023);
+
+        await channel.SendMessageAsync("Bem vindos");
+
+        var embed = new EmbedBuilder();
+        embed.WithImageUrl("");
+        await channel.SendMessageAsync(embed: embed.Build());
+        await channel.SendMessageAsync("");
+
+        await client.DisposeAsync();
+    };
+}
+
+await RunBotAsync();
+Console.ReadKey();
